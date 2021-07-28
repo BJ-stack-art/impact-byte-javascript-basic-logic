504b3f677547cba2ec58b14ce6e4030c674476de
Abdul Latif Mubasir <bj.angel119@gmail.com>
2021-07-28 22:02:51 +0700

    question 2

--- app.js
+++ app.js
@@ -25,3 +25,40 @@
 }
 
 console.log(evenCharacter("pratama")); */
+
+
+
+
+
+
+
+
+
+
+
+// 2. Count Word
+/* 
+    Buatlah sebuah fungsi yang dapat mengembalikan ( return ) jumlah kata yang ada di dalam kalimat,
+    fungsi ini hanya dapat menerima argument tipe data string, dan mengembalikan tipe data number
+
+    "pratama" -> return 1
+    "hellohello" -> return 1
+    "hello hello hello" -> return 3
+    "" -> return 0
+    "hore yeay ahaaa" -> return 3
+    123434 -> return "error input bukan string"
+ */
+
+
+/* const countWord = (string) => {
+    if( typeof string !== 'string' ) return 'error input bukan string';
+    
+    let space = 0;
+    for(let i = 0; i < string.length; i++) {
+        string[i] === " " ? space += 1 : "";
+    }
+    
+    return string.length ? space + 1 : 0;
+} 
+
+console.log(countWord("hello hello hello")) */
