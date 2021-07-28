27c7a85816cf6102e1698bb9ae171c5c387016ac
Abdul Latif Mubasir <bj.angel119@gmail.com>
2021-07-28 22:03:33 +0700

    question 3

--- app.js
+++ app.js
@@ -62,3 +62,47 @@ console.log(evenCharacter("pratama")); */
 } 
 
 console.log(countWord("hello hello hello")) */
+
+
+
+
+
+
+
+
+
+
+// 3. Count Vowel And Consonant
+/* 
+    Buatlah sebuah fungsi yang dapat mengembalikan ( return ) jumlah huruf karakter vokal dan karakter konsonan ( huruf mati ),
+    fungsi ini hanya dapat menerima argument tipe data string, dan mengembalikan tipe data string
+
+    "pratama" -> return "jumlah vokal : 3 , jumlah konsonan : 4"
+    "hellohello" -> return "jumlah vokal : 4, jumlah konsonan : 6"
+    "" -> return "jumlah vokal : 0 , jumlah konsonan 0"
+    123434 -> return "error input bukan string"
+*/
+
+/* const countVowelConsonant = (string) => {
+    if( typeof string !== 'string' ) return 'error input bukan string';
+    
+    let vowel = 0;
+    let consonant = 0;
+
+    const listVowel = ['a','i','u','e','o'];
+    const checkVowel = (character) => {
+        for(let i = 0; i < listVowel.length; i++) {
+            if(listVowel[i] === character) return true;
+        }
+        return false;
+    }
+
+    for(let i = 0; i < string.length; i++) {
+        console.log(string[i] , checkVowel(string[i]));
+        checkVowel(string[i]) ? vowel++ : consonant++;
+    }
+
+    return `jumlah vokal : ${vowel} , jumlah konsonan : ${consonant}`; 
+}
+
+console.log(countVowelConsonant("")); */
